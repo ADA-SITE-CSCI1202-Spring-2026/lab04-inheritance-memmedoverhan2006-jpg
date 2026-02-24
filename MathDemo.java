05b429edede355e92ba561793e6d462ece28b2f4
memmedoverhan2006-jpg <memmedoverhan2006@gmail.com>
2026-02-24 10:53:31 +0400

    Add files via upload

---
+++ MathDemo.java
@@ -0,0 +1,18 @@
+public class MathDemo {
+
+    public static int min(int a, int b) {
+        return (a < b) ? a : b;
+    }
+
+    public static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    public static int sum(int[] args) {
+        int total = 0;
+        for (int num : args) {
+            total += num;
+        }
+        return total;
+    }
+}
